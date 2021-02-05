c5869176e3d969085a41f6fcb96ddd6d957450fb
buyeolim <buyeolim@gmail.com>
2021-02-05 20:36:52 +0900

    Update 'movie_app_prac' about ch4-4~5, ReactJS Fundamentals

--- movie_app_prac/src/Movie.js
+++ movie_app_prac/src/Movie.js
@@ -8,14 +8,14 @@ function Movie({ year, title, summary, poster, genres }) {
       <div>
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <p className="movie__summary">{summary}</p>
-        <ul className="genres">
+        <ul className="movie__genres">
           {genres.map((genre, index) => (
             <li key={index} className="genres__genre">
               {genre}
             </li>
           ))}
         </ul>
+        <p className="movie__summary">{summary.slice(0, 140)}...</p>
       </div>
     </div>
   );
